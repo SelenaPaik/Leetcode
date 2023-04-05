63da7f7f90e4d276b6a8a3f87dd38db9e2f969fa
Selena Paik <ip9911@naver.com>
2023-04-04 23:21:41 -0400

    Time: 171 ms (5.06%), Space: 15 MB (33.73%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,36 @@
+
+#edge case: if string is empty or length is 1, return true
+#intuition: remove spaces, non-alphanumeric characters, convert to lowercase -> check if the letter is same in both sides
+#
+#1. remove non-alphanumeric characters, space
+#2. convert to lowercase
+#3. check from each side(start & end pointer)
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        start, end = 0, len(s)-1
+        s1,s2="",""
+        s = s.lower()
+        if len(s)==0: return True
+        if len(s)==1: return True
+        while start<len(s)-1 and end > 0:
+            if s[start].isalpha() == False and s[start].isnumeric()==False:
+                start +=1
+            if s[end].isalpha() == False and s[end].isnumeric() == False:
+                end -=1
+            print(start, end)
+            if (s[start].isalpha()==True or s[start].isnumeric()==True) and (s[end].isalpha()==True or s[end].isnumeric()==True):
+                s1 += s[start]
+                s2+= s[end]
+                if s[start]==s[end]:
+                    start +=1
+                    end -=1
+                else: 
+                    return False
+        print(s1,s2)
+        if s1==s2:
+            return True
+        else:
+            return False
+        
+               
