7d5890a5e244954994b4f1b4f3ed0525f98c2713
Selena Paik <ip9911@naver.com>
2023-04-05 23:47:52 -0400

    Time: 127 ms (77.54%), Space: 14.8 MB (81.21%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,36 @@
+
+# [First trial]
+# brute force : time limit exceeded
+# 
+# class Solution:
+#     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+#         ans =[]
+#         for i in range(0,len(numbers)):
+#             tmp = target - numbers[i]
+#             for j in range(i+1, len(numbers)):
+#                 if tmp == numbers[j]:
+#                     ans.append(i+1)
+#                     ans.append(j+1)
+#                     return ans
+#         return ans
+# 
+#     
+# [Second trial]
+# 
+# use pointer
+# utilize 'sorted' feature
+# find 2 numbers
+# 
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        r, l = 0, len(numbers)-1
+        while r<l :
+            if numbers[r] + numbers[l] > target:
+                l-=1
+            elif numbers[r] + numbers[l] < target:
+                r+=1
+            elif numbers[r] + numbers[l] == target:
+                return [r+1,l+1]
+        
