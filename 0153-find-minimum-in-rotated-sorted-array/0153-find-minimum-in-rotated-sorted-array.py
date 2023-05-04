7cc1814492656b1347d34be820051bab9f9b2aae
Selena Paik <ip9911@naver.com>
2023-05-04 04:45:41 -0400

    Time: 55 ms (6.62%), Space: 16.7 MB (8.40%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -10,20 +10,43 @@
 #     l = mid+1
 # update res
 
+# class Solution:
+#     def findMin(self, nums: List[int]) -> int:
+#         l,r = 0, len(nums)-1
+#         mid = int((l+r)/2)
+#         res = nums[mid]
+#         
+#         while(l<=r):
+#             if nums[l] < nums[r] : 
+#                 res = min(res, nums[l])
+#                 break
+#                 
+#             mid = int((l+r)/2)
+#             res = min(res, nums[mid])
+# 
+#             if nums[l]> nums[mid]: 
+#                 r = mid-1
+#             else:
+#                 l = mid+1
+# 
+#         return res
+
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         l,r = 0, len(nums)-1
         mid = int((l+r)/2)
         res = nums[mid]
         
+        if nums[l] < nums[r] : return nums[l]
+        
         while(l<=r):
-            if nums[l] < nums[r] : 
-                res = min(res, nums[l])
-                break
-                
             mid = int((l+r)/2)
-            res = min(res, nums[mid])
-
+            if nums[r]<res:
+                res = nums[r]
+            if nums[l]<res:
+                res = nums[l]
+            if nums[mid]<res:
+                res = nums[mid]
             if nums[l]> nums[mid]: 
                 r = mid-1
             else:
