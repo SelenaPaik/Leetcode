481e7c92ea08021b3523e780371270f3681fea93
Selena Paik <ip9911@naver.com>
2023-09-05 00:32:32 -0400

    Time: 39 ms (83.70%), Space: 17.8 MB (72.99%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -3,6 +3,11 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# use 2 pointers, curr and prev
+# 1. make curr.next point prev (we have to reverse it)
+# 2. update: move prev to curr, curr to next
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
