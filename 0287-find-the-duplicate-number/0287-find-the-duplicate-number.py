cd00a1a1c6779037292c4024805b1d523b25591d
Selena Paik <ip9911@naver.com>
2023-12-20 10:55:57 -0500

    Time: 492 ms (98.44%), Space: 39.2 MB (5.13%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,5 +1,8 @@
-#dictionary size = n = len(nums)-1 
-#(int, bool)
+# dictionary size = n = len(nums)-1 
+# (int, bool)
+# 1. Initialize all elements to False
+# 2. If element exist, put True
+# 3. If element already checked(True), return that element
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
